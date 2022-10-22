c968858203bb5fd58ef02115e6ec65a815453cc5
stonka123 <stonka123@gmail.com>
2022-10-22 20:51:26 +0200

    repair styles project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -35,16 +35,15 @@ const Projects = props => {
 					<div className={styles.card}>
 						<div className={styles['card-top']}>
 							<div className={styles['card-back']}>
-								<div className={styles['project-bgc']}></div>
-								<div className={styles['project-link']}>
-									<a href='github.com'> Repozytorium</a>
-									<a href='github.com'> Live</a>
+								<div className={styles['project-bgc']}>
+									<div className={styles['project-link']}>
+										<a href='github.com'> Repozytorium</a>
+										<a href='github.com'> Live</a>
+									</div>
 								</div>
 							</div>
-
 							<img src={ImgProject1} alt='' className={styles['card-img']} />
 						</div>
-
 						<div className={styles.stack}>
 							<SiJavascript className={styles.icon} />
 							<SiGit className={styles.icon} />
