439ceb8ab857774cb4f60ed12264ad49387f2c94
stonka123 <stonka123@gmail.com>
2022-12-23 16:27:09 +0100

    added 4 project and repair width section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import styles from './Projects.module.css'
 import ImgProject1 from '../../assets/img/wheater-projects.jpg'
 import ImgProject2 from '../../assets/img/project2.jpg'
 import ImgProject3 from '../../assets/img/Signerp.jpg'
+import ImgProject4 from '../../assets/img/projekt4.jpg'
 import {
 	SiReact,
 	SiJavascript,
@@ -18,7 +19,7 @@ import { AnimationOnScroll } from 'react-animation-on-scroll'
 
 const Projects = props => {
 	return (
-		<section className={`${styles.projects} wrapper`} id='projects'>
+		<section className={`${styles.projects} `} id='projects'>
 			<AnimationOnScroll animateIn='animate__fadeInRightBig' animateOnce={true}>
 				<svg width='0' height='0'>
 					<linearGradient id='pink-gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
@@ -107,6 +108,30 @@ const Projects = props => {
 								<SiHtml5 className={styles.icon} />
 								<SiSass className={styles.icon} />
 								<SiJavascript className={styles.icon} />
+								<SiGit className={styles.icon} />
+							</div>
+						</div>
+						<div className={styles.card}>
+							<div className={styles['card-top']}>
+								<div className={styles['card-back']}>
+									<div className={styles['project-bgc']}></div>
+									<p>Portfolio page</p>
+									<div className={styles['project-link']}>
+										<a href='https://github.com/stonka123' target='_blank'>
+											{' '}
+											Repository
+										</a>
+										<a href='https://tomasztwarowski.pl/' target='_blank'>
+											{' '}
+											Live
+										</a>
+									</div>
+								</div>
+								<img src={ImgProject4} alt='' className={styles['card-img']} />
+							</div>
+							<div className={styles.stack}>
+								<SiReact className={styles.icon} />
+								<SiAdobephotoshop className={styles.icon} />
 								<SiGit className={styles.icon} />
 							</div>
 						</div>
